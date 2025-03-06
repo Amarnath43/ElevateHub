e37f7602038bdd4ba595d7974d3437d0d5313f81
Amarnath43 <amarnathgoshika@gmail.com>
2025-03-06 14:20:32 +0530

    Updated index.js with base endpoint

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 
 const app=require("./app")
 
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to ElevateHub API"
+        
+    });
+});
+
 app.listen(process.env.PORT, ()=>
 {
     console.log("backend server ")
