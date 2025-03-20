21dcce6f347c51463d2204240b64c1d8a5b191ab
Amarnath43 <amarnathgoshika@gmail.com>
2025-03-20 16:39:12 +0530

    edit profile functionality fixed errors

--- routes/v1/user.routes.js
+++ routes/v1/user.routes.js
@@ -2,8 +2,21 @@ const express=require('express');
 const userController=require('../../controllers/user.controller');
 const authMiddleware=require('../../middleware/auth')
 const asyncHandler=require('../../helper/asyncHandler')
-
 const router=express.Router;
+
+const multer = require('multer'); // Import multer
+
+// Set up multer storage and file filtering options
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); // Naming the file
+  }
+});
+
+const upload = multer({ storage: storage }); // Create the upload middleware
 
 router.post('/upload-photo', authMiddleware.protect, upload.single, asyncHandler(userController.uploadPhoto))
 
