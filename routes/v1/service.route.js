6340a85c9b74d3b522a1eb05090a5e7fc8c29020
Amarnath43 <amarnathgoshika@gmail.com>
2025-03-23 23:26:36 +0530

    implemented service page backend errors

--- routes/v1/service.route.js
+++ routes/v1/service.route.js
@@ -1,27 +1,36 @@
-const express = require('express');
-const serviceController = require("../../controllers/service.controller");
-const asyncHandler = require("../../helper/asyncHandler");
-const validate = require("../../middleware/validate");
-const authMiddleware = require("../../middleware/auth")
+const express=require('express');
+const serviceController=require('../../controllers/service.controller');
+const asyncHandler=require('../../helper/asyncHandler');
+const validate=require('../../middleware/validate')
+const authMiddleware=require('../../middleware/auth');
 const {createServiceSchema}=require('../../validations/service.validation')
 
-const router = express.Router();
-router.post("/", validate(createServiceSchema), authMiddleware.protect, 
-                            authMiddleware.restrictTo("mentor"),
-                            asyncHandler(serviceController.createService))
 
-router.post("/:serviceId", validate(createServiceSchema), authMiddleware.protect, 
-                            authMiddleware.restrictTo("mentor"),
-                            asyncHandler(serviceController.updateService))
+const router=express.Router();
 
-router.get("/", authMiddleware.protect, 
-                            authMiddleware.restrictTo("mentor"),
-                            asyncHandler(serviceController.getServiceByMentor))
+router.post('/',validate(createServiceSchema), authMiddleware.protect, authMiddleware.restrictTo("mentor"), asyncHandler(serviceController.createService));
 
-router.get("/:serviceId", authMiddleware.protect, 
-                                authMiddleware.restrictTo("mentor"),
-                                asyncHandler(serviceController.getServiceById));
+router.put('/:serviceId',validate(createServiceSchema), authMiddleware.protect, authMiddleware.restrictTo("mentor"), asyncHandler(serviceController.updateService))
+
+router.get('/', authMiddleware.protect, authMiddleware.restrictTo("mentor"), asyncHandler(serviceController.getServiceByMentor))
+
+router.get('/:serviceId', authMiddleware.protect, authMiddleware.restrictTo("mentor"), asyncHandler(serviceController.getServiceById))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports=router;
-    
